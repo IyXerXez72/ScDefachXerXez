345b093694e5ec0e9b74bcffdde00d10524c1ff1
InYourXerXez <51115343+IyXerXez72@users.noreply.github.com>
2019-06-04 07:28:42 +0700

    Add files via upload

---
+++ baner.sh
@@ -0,0 +1,21 @@
+b='\033[34;1m'
+g='\033[32;1m'
+p='\033[35;1m'
+c='\033[36;1m'
+r='\033[31;1m'
+w='\033[37;1m'
+y='\033[33;1m'
+sleep 1
+clear
+echo "$b [|]====================================[|]"
+echo "    | $w Author=InYourXerXez             $b|"
+echo "    | $w Team  =2e4h-GblgCrre-Bogor      $b|"
+echo "    | $w Codek By=InYourXerXez7          $b|"
+echo "    | $w since = 2k19-2k20               $b|"
+echo "    | $w                                 $b|"
+echo "    | $w HAY KALO ADA ORANG YANG NDA SUKA$b|"
+echo "    | $w YO BIARIN LAMA LAMA KUGA SUKA   $b|"
+echo "    | $w KALO KAMU TIDA SUKA TOOLS KU ?? $b|"
+echo "    | $w YO OJO NGINSTALL URIP KO RIBET  $b|"
+echo "$b [|]====================================[|]"
+echo "$g"
